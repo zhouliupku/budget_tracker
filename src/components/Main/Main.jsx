b19573fe4fecf888c5f88f94df7f4e53a8afc164
zhouliupku <pkulz@pku.edu.cn>
2022-05-03 22:18:58 -0400

    add form and list

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import { Card, CardHeader, CardContent, Typography, Grid, Divider, List } from '@material-ui/core'
+import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core'
 import useStyles from './styles'
+
+import Form from './Form/Form'
+import List from './List/List'
 
 const Main = () => {
     const classes = useStyles();
@@ -11,12 +14,13 @@ const Main = () => {
                 <Typography align='center' variant='h5'>Total Balance $100</Typography>
                 <Typography variant='subtitle1' style={{lineHeight: '1.5em', marginTop: '20px'}}></Typography>
                 <Divider />
+                <Form />
             </CardContent>
 
             <CardContent className={classes.cartContent}>
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
-                        
+                        <List />  
                     </Grid>
                 </Grid>
 
