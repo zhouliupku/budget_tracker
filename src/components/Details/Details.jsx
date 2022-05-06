f46b4057c1ffd7e283c45dca0228e52fe56d3d74
zhouliupku <pkulz@pku.edu.cn>
2022-05-06 12:34:13 -0400

    doughnut graph debug

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
 import { Doughnut } from 'react-chartjs-2';
 import useStyles from './styles';
+import 'chart.js/auto';
 
 import useTransactions from '../../useTransactions';
 
